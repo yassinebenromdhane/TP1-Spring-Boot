3e7eb92b279750cae6a6900d2a1a6416f5be5ffe
med yassine ben romdhane <yassine.beno@outlook.fr>
2021-10-24 14:27:37 +0100

    first commit

---
+++ src/test/java/com/yassine/produits/ProduitsApplicationTests.java
@@ -0,0 +1,47 @@
+package com.yassine.produits;
+
+import java.util.Date;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.yassine.produits.entities.Produit;
+import com.yassine.produits.repos.ProduitRepository;
+
+@SpringBootTest
+public class ProduitsApplicationTests {
+  @Autowired
+  private ProduitRepository produitRepository;
+
+  @Test
+  public void testCreateProduit() {
+    Produit prod = new Produit("asus", 2200.500, new Date());
+    produitRepository.save(prod);
+  }
+
+  @Test
+  public void testFindProduit() {
+    Produit p = produitRepository.findById(2L).get();
+    System.out.println(p);
+  }
+
+  @Test
+  public void testUpdateProduit() {
+    Produit p = produitRepository.findById(2L).get();
+    p.setPrixProduit(1000.0);
+    produitRepository.save(p);
+  }
+
+  @Test
+  public void testDeleteProduit() {
+    produitRepository.deleteById(2L);
+  }
+
+  @Test
+  public void testListerTousProduits() {
+    List<Produit> prods = produitRepository.findAll();
+    for (Produit p : prods) {
+      System.out.println(p);
+    }
+  }
+}
